1b7219047afa14e16598a750a267a8e65f339260
Umoh Samuel <samuelwisdpom360@gmail.com>
2022-07-27 04:19:52 +0100

    Level 3 - Hashing

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+
 
 const app = express();
 
@@ -17,7 +18,6 @@ const userSchema = mongoose.Schema({
     password:String,
 });
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 
 const User = mongoose.model("User",userSchema);
 
@@ -41,7 +41,7 @@ app.get("/login",(req,res)=>{
 app.post("/register",(req,res)=>{
    const newUser = new User({
        email:req.body.username,
-       password:req.body.password,
+       password:md5(req.body.password)
    });
    newUser.save(err =>{
     if(err){
@@ -54,7 +54,7 @@ app.post("/register",(req,res)=>{
 
 app.post("/login",(req,res)=>{
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({email:username},(err,foundUser)=>{
         if(err){
             console.log(err);
